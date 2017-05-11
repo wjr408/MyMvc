ec1c45991c6b91ffd146f07199ad69bc1a5d450d
wjr408 <921596886@qq.com>
2017-05-11 19:05:38 +0800

    Test为UI项目

---
+++ Test/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using MyMvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Test
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+
+            var defaultPath = new Dictionary<string, object>();
+            defaultPath.Add("controller", "Home");
+            defaultPath.Add("action", "Index");
+            defaultPath.Add("action404", "To404");
+            defaultPath.Add("id", null);
+            defaultPath.Add("namespaces", "Test.Controllers");
+            defaultPath.Add("assembly", "Test");
+            MyRouteTable.Routes.Add("defaultRoute", new MyRoute("{controller}/{action}/{id}", defaultPath, new MyMvc.MVC.MvcRouteHandler()));
+
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
