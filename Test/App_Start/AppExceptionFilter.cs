9ef24452de573fe7a14d1d075dfc6ac64e9283cd
wjr408 <921596886@qq.com>
2017-05-27 17:38:42 +0800

    异常处理类

---
+++ Test/App_Start/AppExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace Test.App_Start
+{
+    public class AppExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>
+        /// 异常事件
+        /// </summary>
+        /// <param name="context">上下文</param>
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is ApplicationException)
+            {
+                //context.Response = context.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, context.Exception.Message);
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
